e27f3fbb8c013cc4fe8a0c01fb331a86d6c1eaa1
Lorenzo Serturini <lorenzo.serturini@gmail.com>
2021-10-22 18:03:34 +0200

    Aggiunto Socket.io

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gridRouter = require('./routes/grid');
 var passRouter = require('./routes/password');
+var socketRouter = require('./routes/socket');
 var cors = require('cors');
 
 var app = express();
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grid', gridRouter);
 app.use('/password', passRouter);
+app.use('/socket', socketRouter);
 
 
 // catch 404 and forward to error handler
---
+++ routes/socket.js
@@ -0,0 +1,35 @@
+const express = require('express');
+var router = express.Router();
+const cors = require('cors');
+const app = express();
+const http = require('http');
+require('events').EventEmitter.defaultMaxListeners = 1000;
+const server = http.createServer(app);
+const io = require('socket.io')(server, { cors: { origin: '*' } });
+
+
+app.get('/', function(req, res) {
+	res.send('HELLO');
+});
+
+
+io.on('connection', (socket) => {
+	console.log(`UTENTE CONNESSO: ${socket.id}`);
+	setInterval(() => {
+		socket.volatile.emit('random', Math.floor((Math.random() * 100) + 1));
+	}, 1000);
+
+	socket.on('disconnect', () => {
+		console.log(`SONO FUORI: ${socket.id}`);
+		delete [socket.id];
+	});
+});
+
+
+server.listen(3001, () => {
+	console.log('listening on *:3001');
+});
+
+module.exports = router;
+
+
